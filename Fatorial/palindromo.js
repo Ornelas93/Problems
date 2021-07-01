2f61641ccec173246eed2f0aecca644279837089
Raysson Ornelas de Andrade <rayssonornelas2006@gmail.com>
2021-07-01 16:06:07 -0300

    Tentativa no problema palindromo

--- Fatorial/palindromo.js
+++ Fatorial/palindromo.js
@@ -1,15 +1,15 @@
 const palavraPalindromo = "A rua Laura";
 const convertePalindromo = (palavraPalindromo) =>{
     var palindromo = "";
-    for(let i = 0; i < palavraPalindromo.length -1; i ++){
-      for(let c = palavraPalindromo.length -1; c >= 0; c--){
-          if(palavraPalindromo[i] != palavraPalindromo[c]){
-              if(palavraPalindromo[c] != " "){
-                palindromo += palavraPalindromo[c];
-              }
-              else
-          }
-          
+    var teste = palavraPalindromo.trim();
+    var array = [...teste];
+    var countChar = palavraPalindromo.length -1;
+    for(let i = countChar, j = 0; i >= 0  ; i--, j++){
+      if (palavraPalindromo[j] === " "){
+        palindromo += " " + array[i];
+      }
+      else {
+        palindromo += array[i];
       }
     }
     return palindromo;
