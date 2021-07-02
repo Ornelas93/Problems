b2eb83f3d1d7d8cd4b58660260ab35c99adda5f3
Ornelas93 <rayssonornelas2006@gmail.com>
2021-07-01 22:07:47 -0300

    Mais uma tentativa no palindromo

--- Fatorial/palindromo.js
+++ Fatorial/palindromo.js
@@ -1,17 +1,14 @@
 const palavraPalindromo = "A rua Laura";
 const convertePalindromo = (palavraPalindromo) =>{
-    var palindromo = "";
-    var teste = palavraPalindromo.trim();
-    var array = [...teste];
-    var countChar = palavraPalindromo.length -1;
-    for(let i = countChar, j = 0; i >= 0  ; i--, j++){
-      if (palavraPalindromo[j] === " "){
-        palindromo += " " + array[i];
-      }
-      else {
-        palindromo += array[i];
-      }
+    var palindromo="";
+    palavraPalindromo = palavraPalindromo.toLowerCase();
+    var teste = [...palavraPalindromo.replace(/\s/g, '')];
+    var array =[...palavraPalindromo];
+    var countChar = teste.length-1;
+    for(let i = countChar, j = 0; i >= 0 ; i--, j++){
+      console.log(teste[i],array[j]);
     }
-    return palindromo;
+    
 }
-console.log(convertePalindromo(palavraPalindromo));
+convertePalindromo(palavraPalindromo);
+//console.log(convertePalindromo(palavraPalindromo));
