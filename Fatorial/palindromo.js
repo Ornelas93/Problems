ac34dfa3525bf16fc9abaeee289dbb87bf2d589a
Ornelas93 <rayssonornelas2006@gmail.com>
2021-07-05 20:15:38 -0300

    Resolução do problema de palindromo

--- Fatorial/palindromo.js
+++ Fatorial/palindromo.js
@@ -1,14 +1,16 @@
-const palavraPalindromo = "A rua Laura";
+const palavraPalindromo = "Raysson";
+
 const convertePalindromo = (palavraPalindromo) =>{
-    var palindromo="";
-    palavraPalindromo = palavraPalindromo.toLowerCase();
-    var teste = [...palavraPalindromo.replace(/\s/g, '')];
-    var array =[...palavraPalindromo];
-    var countChar = teste.length-1;
-    for(let i = countChar, j = 0; i >= 0 ; i--, j++){
-      console.log(teste[i],array[j]);
-    }
-    
+  var er = /[^a-z0-9]/gi;
+  var array =[...palavraPalindromo.replace(er,"")];
+  var array2 = [...palavraPalindromo.replace(er,"")];
+  array = array.reverse();
+  console.log(array2, array);
+  var is_same = (array.length == array2.length) && array.every(function(element, index) {
+    return element === array2[index]; 
+});
+  
+  console.log(is_same);
 }
-convertePalindromo(palavraPalindromo);
+convertePalindromo(palavraPalindromo.toUpperCase());
 //console.log(convertePalindromo(palavraPalindromo));
