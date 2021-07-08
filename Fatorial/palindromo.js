92189d8b6c8664754aee0736f4e198ee8cc5d00f
Raysson Ornelas de Andrade <rayssonornelas2006@gmail.com>
2021-07-08 17:17:40 -0300

    Correção no problema do palindromo

--- Fatorial/palindromo.js
+++ Fatorial/palindromo.js
@@ -1,15 +1,13 @@
-const palavraPalindromo = "Raysson";
+const palavraPalindromo = "AMOR A ROMA";
 
 const convertePalindromo = (palavraPalindromo) =>{
-  var er = /[^a-z0-9]/gi;
+  var er = /[^a-zA-Zs]/g;
   var array =[...palavraPalindromo.replace(er,"")];
   var array2 = [...palavraPalindromo.replace(er,"")];
   array = array.reverse();
-  console.log(array2, array);
   var is_same = (array.length == array2.length) && array.every(function(element, index) {
-    return element === array2[index]; 
+    return element === array2[index];
 });
-  
   console.log(is_same);
 }
 convertePalindromo(palavraPalindromo.toUpperCase());
