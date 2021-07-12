d08d39616e06f310087f17c59a0bbacd3003b2f1
Ornelas93 <rayssonornelas2006@gmail.com>
2021-07-12 20:49:37 -0300

    Correções no problema das palavras primas e dos palindromos

--- Fatorial/palindromo.js
+++ Fatorial/palindromo.js
@@ -1,14 +1,24 @@
-const palavraPalindromo = "AMOR A ROMA";
+/*
+- Nunca use var(usar let ou const)
+- Se eu utilizar var tem que pagar açai
+- Estudar Função split
+- Ficar atento ao nome das variaveis(ser Descritivas)
+- Fazer pergunta ao receber o problema
+- Fazer usando um for e verificando as duas pontas ao mesmo tempo
+- usar sem pre o padrao camelCase
+*/
 
-const convertePalindromo = (palavraPalindromo) =>{
-  var er = /[^a-zA-Zs]/g;
-  var array =[...palavraPalindromo.replace(er,"")];
-  var array2 = [...palavraPalindromo.replace(er,"")];
+const testCase = "AMOR A ROMA";
+
+const isPalindromo = (palavraPalindromo) =>{
+  var removeCaraterEspeciais = /[^a-zA-Zs]/g;
+  var array =[...palavraPalindromo.replace(removeCaraterEspeciais,"")];
+  var array2 = [...palavraPalindromo.replace(removeCaraterEspeciais,"")];
   array = array.reverse();
   var is_same = (array.length == array2.length) && array.every(function(element, index) {
     return element === array2[index];
 });
   console.log(is_same);
 }
-convertePalindromo(palavraPalindromo.toUpperCase());
+isPalindromo(testCase.toUpperCase());
 //console.log(convertePalindromo(palavraPalindromo));
